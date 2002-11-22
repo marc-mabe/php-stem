28d057e4b5fe4479aba0836ee3178636558c708f
jay <jay@c90b9560-bf6c-de11-be94-00142212c4b1>
2002-11-22 18:36:32 +0000

    Added the Finnish stemmer files and the config header.
    git-svn-id: https://svn.php.net/repository/pecl/stem/trunk@105324 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ finnish.h
@@ -0,0 +1,8 @@
+
+/* This file was generated automatically by the Snowball to ANSI C compiler */
+
+extern struct SN_env * finnish_create_env(void);
+extern void finnish_close_env(struct SN_env * z);
+
+extern int finnish_stem(struct SN_env * z);
+
---
+++ stemconf.h
@@ -0,0 +1,38 @@
+/*
+   +----------------------------------------------------------------------+
+   | Copyright (c) 2002 J Smith <dark_panda@hushmail.com>                 |
+   | Snowball sources copyright (c) M.F. Porter                           |
+   +----------------------------------------------------------------------+
+   | Check out COPYING for the exact license.                             |
+   +----------------------------------------------------------------------+
+
+ */
+
+#ifndef __STEMCONF_H__
+#define __STEMCONF_H__
+
+// Here's where you can disable the algorithms.
+// Set these to 1 to enable, 0 to disable. By
+// default, they're all enabled. The original
+// Porter stemming algorithm is always enabled.
+
+
+// English -- sort of a modified Porter...
+
+#define ENABLE_ENGLISH		1
+
+// The rest are what they say...
+
+#define ENABLE_DANISH		1
+#define ENABLE_DUTCH		1
+#define ENABLE_FINNISH		1
+#define ENABLE_FRENCH		1
+#define ENABLE_GERMAN		1
+#define ENABLE_ITALIAN		1
+#define ENABLE_NORWEGIAN	1
+#define ENABLE_PORTUGUESE	1
+#define ENABLE_RUSSIAN		1
+#define ENABLE_SPANISH		1
+#define ENABLE_SWEDISH		1
+
+#endif
