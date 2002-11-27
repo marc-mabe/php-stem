f8e71591c43c5954c1c311913029bfdc5df63c38
jay <jay@c90b9560-bf6c-de11-be94-00142212c4b1>
2002-11-27 20:17:32 +0000

    Full tests...!
    git-svn-id: https://svn.php.net/repository/pecl/stem/trunk@106078 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ tests/fulltests.php
@@ -0,0 +1,32 @@
+<?php
+
+function runTest($lang, $in, $out, $gz = true)
+{
+    if ($gz)
+    {
+        $in = gzfile("$in.gz");
+        $out = gzfile("$out.gz");
+    }
+    else
+    {
+        $in = file($in);
+        $out = file($out);
+    }
+    $bad = 0;
+
+    for ($i = 0; $i < count($in); $i++)
+    {
+        $in[$i] = trim($in[$i]);
+        $out[$i] = trim($out[$i]);
+
+        if (stem($in[$i], $lang) != $out[$i])
+        {
+            print "|" . $in[$i] . "| -> |" . stem($in[$i], $lang) . "|, expected |" . $out[$i] . "|\n";
+            ++$bad;
+        }
+    }
+
+    print "BAD: $bad\n";
+}
+
+?>
