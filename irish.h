6ca4597ea7a19050d5d8d4ebcfb2affc39482c5c
Marc Bennewitz <marc-bennewitz@arcor.de>
2013-05-03 08:41:33 +0200

    Added missing irish.c and irish.h files

---
+++ irish.h
@@ -0,0 +1,16 @@
+
+/* This file was generated automatically by the Snowball to ANSI C compiler */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern struct SN_env * irish_create_env(void);
+extern void irish_close_env(struct SN_env * z);
+
+extern int irish_stem(struct SN_env * z);
+
+#ifdef __cplusplus
+}
+#endif
+
